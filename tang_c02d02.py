ab8e47668ca04812e6e62d2fce568df1594574a8
twjin <jinartphoto@gmail.com>
2015-01-23 16:54:14 +0800

    fix typo and clearly specify meaning of  each code
    learning python syntaxt

--- tang_c02d02.py
+++ tang_c02d02.py
@@ -1,10 +1,10 @@
 # File: Chaos.py # this is a comment
-#   A simple program illustrating chaotic behavior. # this is a commnet
+#   A simple program illustrating chaotic behavior. # this is a comment
 
-def main(): # this is an assignment
-    print("This program illustrates a chaotic function") # this is an assignment
-    x = eval(input("Enter a number between 0 and 1:") # this is an input
-    for i in rang (10): # this is loop
+def main(): # this is a function definition
+    print("This program illustrates a chaotic function") # this is a function that output a string
+    x = eval(input("Enter a number between 0 and 1:") # this is an assignment that accepts an user input
+    for i in range (10): # this is a for loop
              x = 3.9*x*(1-x)  # this is an assignment
-             print (x) # this is an output
+             print (x) # this is a function that generate the final output of the script
              
