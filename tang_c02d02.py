3e512f0dc0b790fb710a207368151ee62bd37631
twjin <jinartphoto@gmail.com>
2015-01-23 16:41:39 +0800

    hw2
    read code and add comment

---
+++ tang_c02d02.py
@@ -0,0 +1,10 @@
+# File: Chaos.py # this is a comment
+#   A simple program illustrating chaotic behavior. # this is a commnet
+
+def main(): # this is an assignment
+    print("This program illustrates a chaotic function") # this is an assignment
+    x = eval(input("Enter a number between 0 and 1:") # this is an input
+    for i in rang (10): # this is loop
+             x = 3.9*x*(1-x)  # this is an assignment
+             print (x) # this is an output
+             
